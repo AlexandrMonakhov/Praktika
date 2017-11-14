9fc7cedde16032de95b1663ec58c1f11e124c763
AlexandrMonakhov <a.monahov@mail.ru>
2017-11-14 23:53:50 +0400

    1

--- js/script.js
+++ js/script.js
@@ -23,20 +23,51 @@ function hide1()
 function show2()
 {
 	var open2 = document.querySelector(".products_info");
-	open2.style.display = "block";
+	open2.style.display = "flex";
+}
+function hide2()
+{
+	var open2 = document.querySelector(".products_info");
+	open2.style.display = "none";
 }
 function show3()
 {
-	var open3 = document.querySelector(".products_info_1");
-	open3.style.display = "block";
+	var open2 = document.querySelector(".products_info1");
+	open2.style.display = "flex";
+}
+function hide3()
+{
+	var open2 = document.querySelector(".products_info1");
+	open2.style.display = "none";
 }
 function show4()
 {
-	var open4 = document.querySelector(".products_info_2");
-	open4.style.display = "block";
+	var open2 = document.querySelector(".products_info2");
+	open2.style.display = "flex";
 }
+function hide4()
+{
+	var open2 = document.querySelector(".products_info2");
+	open2.style.display = "none";
+}
+
 function show5()
 {
-	var open5 = document.querySelector(".products_info_3");
-	open5.style.display = "block";
+	var open2 = document.querySelector(".products_info3");
+	open2.style.display = "flex";
 }
+function hide5()
+{
+	var open2 = document.querySelector(".products_info3");
+	open2.style.display = "none";
+}
+
+function sum()
+{
+k=0;
+for(i=0;i<6;i++)
+{if(document.getElementById('tovar'+i).checked) k+=parseInt(document.getElementById('tovar'+i).value);}
+document.form1.res.value=k;
+}
+
+
